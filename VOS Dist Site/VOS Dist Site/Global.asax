51f2332c82f7fbcdfee8e801fada75c6acf311d3
BudHL <bud.long196@gmail.com>
2016-10-02 22:43:26 -0400

    Test Commit

---
+++ VOS Dist Site/VOS Dist Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VOS_Dist_Site.MvcApplication" Language="C#" %>
---
+++ VOS Dist Site/VOS Dist Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VOS_Dist_Site.Startup))]
+namespace VOS_Dist_Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
