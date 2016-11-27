13568229e6246d23a8e9a390b2c5b582764738f5
Bud Long <bud.long196@gmail.com>
2016-11-27 18:51:57 -0500

    Fixed Db, Working on CSS

---
+++ VOS Dist Site/VOS Dist Site/Migrations/201611272333333_migrationconfigUpdate.cs
@@ -0,0 +1,16 @@
+namespace VOS_Dist_Site.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migrationconfigUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- VOS Dist Site/VOS Dist Site/Migrations/Configuration.cs
+++ VOS Dist Site/VOS Dist Site/Migrations/Configuration.cs
@@ -4,6 +4,15 @@ namespace VOS_Dist_Site.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Web;
+    using VOS_Dist_Site.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<VOS_Dist_Site.Models.ApplicationDbContext>
     {
@@ -14,6 +23,19 @@ namespace VOS_Dist_Site.Migrations
 
         protected override void Seed(VOS_Dist_Site.Models.ApplicationDbContext context)
         {
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var user = new ApplicationUser { UserName = "admin@gmail.com" };
+            var guestUser = new ApplicationUser { UserName = "guest@guest.com" };
+
+            userManager.Create(user, "abc123"); //strong password!#@$!
+            userManager.Create(guestUser, "guest1"); //strong password!#@$!
+
+            roleManager.Create(new IdentityRole { Name = "Admin" });
+            userManager.AddToRole(user.Id, "Admin");
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
